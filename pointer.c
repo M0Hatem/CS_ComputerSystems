957361d76dbb3fd350e5b119b1c9aba22d59107e
Mohamed <moh.medh.tem1@gmail.com>
2024-06-06 06:59:03 +0300

    opinter.c

---
+++ pointer.c
@@ -0,0 +1,20 @@
+//
+// Created by Mohamed Hatem on 06/06/2024.
+//
+#include <stdio.h>
+
+int main (int argc,char *argv[]) {
+    int a[10];
+    for(int i = 0;i <10;i++) {
+        a[i]=i;
+    }
+
+    int *p;
+    p=&a[0];
+    for (int i = 0; i < 10; ++i) {
+
+        printf("%d \n",a[i]);
+        printf("%d \n",*p++);
+        printf("%d \n",a[1]);
+    }
+}
